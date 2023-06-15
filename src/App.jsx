e369c206217183283e1e70dd8a848ead15751f41
Matt-Hulme <Mattwhulme@gmail.com>
2023-06-15 08:16:56 -0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Calculator from "./components/Calculator.jsx"
+
+export default function App() {
+
+  return ( 
+    <div className="HomePage">
+      <Calculator/>
+    </div>
+  )
+}
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Button from './Button';
+
+export default function Calculator() {
+  const handleMultiplication = () => {
+    // Handle multiplication logic
+  };
+
+  const handleAddition = () => {
+    // Handle addition logic
+  };
+
+  const handleDivision = () => {
+    // Handle division logic
+  };
+
+  return (
+    <div className="HomePage">
+      <div className="Calculator">
+      Calculator
+        <div className="Screen">Screen</div>
+        <div className="MemoryRow">Memory Row</div>
+        <div className="ButtonsGrid">Buttons Grid</div>
+      </div>
+    </div>
+    
+  );
+}
+
