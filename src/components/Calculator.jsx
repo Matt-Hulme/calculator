5d28c33b8454307a84add402f9ee74d83b239791
Matt-Hulme <Mattwhulme@gmail.com>
2023-06-16 15:08:15 -0600

    ButtonLayout1

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -20,7 +20,32 @@ export default function Calculator() {
       Calculator
         <div className="Screen">Screen</div>
         <div className="MemoryRow">Memory Row</div>
-        <div className="ButtonsGrid">Buttons Grid</div>
+        <div className="ButtonsGrid">
+          <div className="Button1">Button1</div>
+          <div className="Button2">Button2</div>
+          <div className="Button3">Button3</div>
+          <div className="Button4">Button4</div>
+          <div className="Button5">Button5</div>
+          <div className="Button6">Button6</div>
+          <div className="Button7">Button7</div>
+          <div className="Button8">Button8</div>
+          <div className="Button9">Button9</div>
+          <div className="Button10">Button10</div>
+          <div className="Button11">Button11</div>
+          <div className="Button12">Button12</div>
+          <div className="Button13">Button13</div>
+          <div className="Button14">Button14</div>
+          <div className="Button15">Button15</div>
+          <div className="Button16">Button16</div>
+          <div className="Button17">Button17</div>
+          <div className="Button18">Button18</div>
+          <div className="Button19">Button19</div>
+          <div className="Button20">Button20</div>
+          <div className="Button21">Button21</div>
+          <div className="Button22">Button22</div>
+          <div className="Button23">Button23</div>
+          <div className="Button24">Button24</div>
+        </div>
       </div>
     </div>
     
