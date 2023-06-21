ce09aed3af0c79648d9164a8b440fbff0aedbf3c
Matt-Hulme <Mattwhulme@gmail.com>
2023-06-20 20:35:20 -0600

    BackSpace is Broken :'(

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,138 +1,120 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from './Button.jsx';
 import { evaluate } from 'mathjs';
 
 export default function Calculator() {
-  const [input, setInput] = useState(0);
-  const [equation, setEquation] = useState('');
-  const [displayEquation, setDisplayEquation] = useState('');
-  const [result, setResult] = useState(0);
+  const [ input, setInput] = useState(0);
+  const [ result, setResult] = useState(0);
+  const [ equation, setEquation] = useState('');
+  const [ equationDisplay, setEquationDisplay] = useState('');
+  const [ history, setHistory] = useState([]);
 
+  
   const handleNumeric = (number) => {
-    setInput((prevInput) => prevInput * 10 + number);
+    setInput((prevInput) => {
+      let newInput;
+      if (prevInput === 0 || !prevInput.toString().includes('.')) {
+        newInput = prevInput * 10 + number;
+      } else {
+        newInput = parseFloat(prevInput.toString() + '.' + number.toString());
+      }
+      const newEquation = equation + number;
+      setResult(evaluate(newEquation));
+      setEquation(newEquation);
+      return newInput;
+    });
   };
-
-  const handleCE = () => {
-    setInput(0);
+  
+  
+  
+  
+  
+  
+  
+  const handleDecimal = () => {
+    if (Number.isInteger(input)) {
+      setInput((prevInput) => prevInput + '.');
+      setEquation((prevEquation) => prevEquation + '.')
+    }
   };
+  
 
   const handleC = () => {
     setInput(0);
     setResult(0);
     setEquation('');
-    setDisplayEquation('');
+    setEquationDisplay('');
   };
 
   const handleBackspace = () => {
     setInput((prevInput) => {
-      let inputString = prevInput.toString();
-      let newInputString = inputString.slice(0, -1);
-      return newInputString ? parseFloat(newInputString) : 0;
+      const inputString = prevInput.toString();
+      const newInputString = inputString.slice(0, -1);
+      const newInput = newInputString ? parseFloat(newInputString) : 0;
+  
+      setEquation((prevEquation) => {
+        const lastCharIndex = prevEquation.length - 1;
+        let newEquation = prevEquation;
+        if (prevEquation.charAt(lastCharIndex) === ' ') {
+          newEquation = prevEquation.slice(0, lastCharIndex - 2);
+        } else {
+          newEquation = prevEquation.slice(0, lastCharIndex);
+        }
+        setResult(evaluate(newEquation));
+        return newEquation;
+      });
+  
+      return newInput;
     });
   };
+  
+  
 
+  
+  
   const handleMultiply = () => {
-    setInput(0);
-    setEquation((prevEquation) => prevEquation.concat(` ${input} * `));
-    setDisplayEquation((prevDisplayEquation) => prevDisplayEquation.concat(` ${input} *`));
   };
 
   const handleDivide = () => {
-    setInput(0);
-    setEquation((prevEquation) => prevEquation.concat(` ${input} / `));
-    setDisplayEquation((prevDisplayEquation) => prevDisplayEquation.concat(` ${input} /`));
   };
 
   const handleSubtract = () => {
-    setInput(0);
-    setEquation((prevEquation) => prevEquation.concat(` ${input} - `));
-    setDisplayEquation((prevDisplayEquation) => prevDisplayEquation.concat(` ${input} -`));
   };
 
   const handleAdd = () => {
-    setInput(0);
-    setEquation((prevEquation) => prevEquation.concat(` ${input} + `));
-    setDisplayEquation((prevDisplayEquation) => prevDisplayEquation.concat(` ${input} +`));
-  };
-
-  const handleNegate = () => {
-    setInput((prevInput) => prevInput * -1);
-  };
-
-  const handleDecimal = () => {
-    if (Number.isInteger(input)) {
-      setInput((prevInput) => prevInput + '.');
+    if (!(equation.slice(-2).includes("+"))){
+      setInput(0);
+      setEquation((prevEquation) => prevEquation.concat(' + '));
     }
   };
+  
+  const handleNegate = () => {
+  };
 
 
   const handleEquals = () => {
-   
-
-
-
-  const handleEquals = () => {
-    setEquation((prevEquation) => {
-      let updatedEquation = prevEquation + input;
-      let evalResult = evaluate(updatedEquation);
-      setResult(evalResult);
-      setInput(evalResult.toString());
-      return evalResult.toString();
-    });
-  
-    setDisplayEquation((prevDisplayEquation) => {
-      const updatedDisplayEquation = prevDisplayEquation + ' ' + input + ' =';
-      return updatedDisplayEquation;
-    });
-    
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  // const handleEquals = () => {
-  //   setEquation((prevEquation) => {
-      
-  //   });
-  
-  //   setDisplayEquation((prevDisplayEquation) => {
-
-  //   });
-  // };
+  };
   
   
   
-  
+
   return (
     <div className="Calculator">
       Calculator
       <div className="Screen">
-        <div className="ScreenResult">
-          {input}
-        </div>
-        <div className="ScreenEquation">{displayEquation}</div>
+        <div className="ScreenResult">{result}</div>
+        <div className="ScreenEquation">{equation}</div>
       </div>
       <div className="MemoryRow">Memory Row</div>
+      <div className ="ClearRow" onClick={() => handleC()}>Clear</div>
       <div className="ButtonsGrid">
         <Button label="%" onClick={() => handlePercentage()}></Button>
-        <Button label="CE" onClick={() => handleCE()}></Button>
-        <Button label="C" onClick={() => handleC()}></Button>
+        <Button label="("></Button>
+        <Button label=")"></Button>
         <Button label="Backspace" onClick={() => handleBackspace()}></Button>
         <Button label="1/X" onClick={() => handleOneByX()}></Button>
-        <Button label="x^2" onClick={() => handleSquared()}></Button>
-        <Button label="2√x" onClick={() => handleRootX()}></Button>
+        <Button label="X^2" onClick={() => handleSquared()}></Button>
+        <Button label="2√X" onClick={() => handleRootX()}></Button>
         <Button label="/" onClick={() => handleDivide()}></Button>
         <Button label="7" onClick={() => handleNumeric(7)}></Button>
         <Button label="8" onClick={() => handleNumeric(8)}></Button>
